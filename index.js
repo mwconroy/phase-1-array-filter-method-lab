f79ca997fc82fa948180d396a3ef71443e6f3fe6
Mike <mikewconroy3@gmail.com>
2021-11-22 18:34:48 -0600

    commit tree 8fc9d66af0e98094de841dca36e8f149e126bdf9

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+
+function findMatching(drivers, name){
+    return drivers.filter(function (Name){ 
+        return Name.toLowerCase() === name.toLowerCase()
+    })
+}
+function matchName(drivers, name){
+  return drivers.filter( city => city.name === name)
+}
+
+  function fuzzyMatch(nameList, partOfName){
+    let name = partOfName.length
+      return nameList.filter(function (nameOfDriver){
+          return nameOfDriver.slice(0, name) ===partOfName
+      })
+  }
